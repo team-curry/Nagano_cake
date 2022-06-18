33571add199965a55b1472e8976e631fb89da4d3
EC2 Default User <ec2-user@ip-172-31-25-66.ec2.internal>
2022-06-18 08:26:38 +0000

    [Update]devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,17 @@
 Rails.application.routes.draw do
-  
-  
+
+
   namespace :admins do
-  
+
   end
   # devise_for :customers
   # devise_for :admins
-  devise_for :admins, controllers: {
-  sessions:      'admins/sessions',
-  passwords:     'admins/passwords',
-  registrations: 'admins/registrations'
+  devise_for :admins, skip: [:registrations, :passwords] ,controllers: {
+  sessions:      'admins/sessions'
 }
-devise_for :customers, controllers: {
-  sessions:      'customers/sessions',
-  passwords:     'customers/passwords',
-  registrations: 'customers/registrations'
+devise_for :customers, skip: [:passwords] ,controllers: {
+  registrations: 'customers/registrations',
+  sessions:      'customers/sessions'
 }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
