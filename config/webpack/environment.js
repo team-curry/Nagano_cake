ec3a9db2ac487b8f972aa552dadbb1a4774e8aeb
kuma63 <t09min25.y@gmail.com>
2022-06-19 09:07:16 +0000

    [fix]conflick

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,19 +5,11 @@ const { environment } = require('@rails/webpacker')
 module.exports = environment
 
 const webpack = require('webpack')
-<<<<<<< HEAD
  environment.plugins.prepend(
-=======
-environment.plugins.prepend(
->>>>>>> develop2
   'Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
     jQuery: 'jquery/src/jquery',
     Popper: 'popper.js'
   })
-<<<<<<< HEAD
  )
-=======
-)
->>>>>>> develop2
