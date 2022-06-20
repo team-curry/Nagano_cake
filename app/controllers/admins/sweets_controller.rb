e1d2ece5b8f371466f0cd340e2452f3768b1fc57
kuma63 <t09min25.y@gmail.com>
2022-06-20 08:44:51 +0000

    [add]sweet_new_show

--- app/controllers/admins/sweets_controller.rb
+++ app/controllers/admins/sweets_controller.rb
@@ -1,3 +1,25 @@
 class Admins::SweetsController < ApplicationController
     layout "admins"
+    
+ def new
+  @sweet = Sweet.new
+  @genres = Genre.all
+ end
+ 
+ def create
+  @sweet = Sweet.new(sweet_params)
+  @sweet.save
+  redirect_to admins_sweet_path(@sweet.id)
+ end
+ 
+ def show
+  @sweet = Sweet.find(params[:id])
+  @genre = @sweet.genre
+ end
+ 
+ private
+ 
+ def sweet_params
+  params.require(:sweet).permit(:genre_id, :name, :explanation, :is_sale_status, :price, :image)
+ end
 end
