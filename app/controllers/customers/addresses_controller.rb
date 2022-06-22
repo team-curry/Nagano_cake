7e4adef6c3ee210f7bf9f5b58b9465140d463ce3
EC2 Default User <ec2-user@ip-172-31-25-66.ec2.internal>
2022-06-22 06:32:38 +0000

    [Add] customers/addresses_controller

--- app/controllers/customers/addresses_controller.rb
+++ app/controllers/customers/addresses_controller.rb
@@ -1,3 +1,15 @@
 class Customers::AddressesController < ApplicationController
   layout "customers"
+  def index
+    @addresses = Address.all
+    @address = Address.new
+  end
+  def edit
+    @address = Address.find(params[:id])
+    if @customer == current_customer
+      render :edit
+    else
+      redirect_to customer_path(current_customer.id)
+    end
+  end
 end
