6e90924472b786625cf34adfede8a171222a1123
say-yama <muhaku.124321@gmail.com>
2022-06-22 05:48:59 +0000

    order-new

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -1,16 +1,21 @@
 class Customers::OrdersController < ApplicationController
 layout "customers"
+
+    def new
+      @order = Order.new
+    end
   
-  def index
+    def create
+      redirect_to action: :complete
+    end
     
-  end
-  
-  def create
-    redirect_to action: :complete
-  end
-  
-  def complete
+    def complete
+      
+    end
     
-  end
+    def index
+      
+    end
+    
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
      collection do
       get 'complete'
      end
+     collection do
+      post 'confirm'
+    end
   end
   resources:addresses
 
