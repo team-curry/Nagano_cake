9899a3fd4bc021a190235d8e939adba1e5f18aaa
kuma63 <t09min25.y@gmail.com>
2022-06-22 05:20:58 +0000

    [add]customer_index

--- app/controllers/admins/sweets_controller.rb
+++ app/controllers/admins/sweets_controller.rb
@@ -18,8 +18,8 @@ class Admins::SweetsController < ApplicationController
  end
  
  def index
-  @sweets = Sweet.page(params[:page])
-  @genre = Genre.page(params[:page])
+  @sweets = Sweet.page(params[:page]).per(5)
+  @genre = Genre.page(params[:page]).per(5)
  end 
  
  def edit
