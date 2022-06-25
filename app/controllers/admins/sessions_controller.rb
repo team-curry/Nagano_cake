3434c48868ba25cd74625857754922720b55195b
kuma63 <t09min25.y@gmail.com>
2022-06-25 12:16:19 +0000

    [fix]

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -26,7 +26,7 @@ class Admins::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
   def after_sign_in_path_for(resource)
-    admins_orders_path
+    admins_path
   end
 
   def after_sign_out_path_for(resource)
