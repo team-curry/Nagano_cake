74d254a42e13229e3663f8a084fed4d129abcaf8
EC2 Default User <ec2-user@ip-172-31-25-66.ec2.internal>
2022-06-25 09:15:14 +0000

    [Fix]conflict?

--- app/controllers/customers/customers_controller.rb
+++ app/controllers/customers/customers_controller.rb
@@ -5,24 +5,5 @@ class Customers::CustomersController < ApplicationController
    @customer = Customer.find(params[:id])
   end
 
-  def edit
-    @customer = Customer.find(params[:id])
-  end
-
-  def update
-    @customer = Customer.find(params[:id])
-    if @customer.update(customer_params)
-      redirect_to customers_customer_path(@customer.id)
-    else
-      render :edit
-    end
-  end
-
-  def withdrawal
-    @customer = Customer.find(params[:id])
-    @customer.update(is_deleted: true)
-    reset_session
-    redirect_to root_path
-  end
 
 end
